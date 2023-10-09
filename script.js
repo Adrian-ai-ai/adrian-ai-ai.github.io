accbe4dfd32278cfc1b5a9f0a38087759f9930ae
adrian.disonest <120581795+Adrian-ai-ai@users.noreply.github.com>
2023-10-09 18:50:04 +0200

    commit4
    updated navigation bar to click

---
+++ script.js
@@ -0,0 +1,6 @@
+// window.onClick=function(event){
+//     if(!event.target.matches('.act')){
+//         let drop=document.querySelector('.sub-links-ul');
+//         drop.style.display='none';
+//     }
+// }
